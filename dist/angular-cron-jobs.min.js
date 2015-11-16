2dabbaf0704659cc64c2854e2222a30888e00bc8
Petró Tamás <petro.tamas@gmail.com>
2015-11-16 13:46:21 +0100

    css fix

--- dist/angular-cron-jobs.min.js
+++ dist/angular-cron-jobs.min.js
@@ -1,6 +1,6 @@
 /**
  * UI Component For Creating Cron Job Syntax To Send To Server
- * @version v1.4.1 - 2015-11-15 * @link https://github.com/jacobscarter/angular-cron-jobs
+ * @version v1.4.1 - 2015-11-16 * @link https://github.com/jacobscarter/angular-cron-jobs
  * @author Jacob Carter <jacob@ieksolutions.com>
  * @license MIT License, http://www.opensource.org/licenses/MIT
  */
